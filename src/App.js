c69a4073911b4d4884fdc47377ff33e11565b13c
Julian <105754345+jprune@users.noreply.github.com>
2022-10-08 11:34:05 +0200

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const App = () => (
-  <div>
-    App
-  </div>
-);
+const axios = require('axios').default;
+
+const App = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_CNYoNRn32rpw0rYfKdkIVLLn46bbK');
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      App
+    </div>
+  );
+};
 
 export default App;
